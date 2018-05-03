75526e70cec4e29df2f4ed010b40587d07291702
yushaosen <yushaosen@163.com>
2018-05-03 17:33:30 +0800

    link-rel

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="public/css/app.css">
   </head>
   <body>
     <header class="navbar navbar-fixed-top navbar-inverse">
