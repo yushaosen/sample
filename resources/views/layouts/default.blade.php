2a8e87a48aa95098cd47ed07db255d9015cf0e9b
yushaosen <yushaosen@163.com>
2018-05-04 17:07:37 +0800

    include

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,26 +1,16 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <title>@yield('title', 'Sample App') - Laravel</title>
     <link rel="stylesheet" href="/public/css/app.css">
   </head>
   <body>
-    <header class="navbar navbar-fixed-top navbar-inverse">
-      <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-          <a href="/public" id="logo">Sample App</a>
-          <nav>
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="/public/help">帮助</a></li>
-              <li><a href="#">登录</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
-
+    @include('layouts._header')
     <div class="container">
-      @yield('content')
+        <div class="col-md-offset-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
     </div>
   </body>
 </html>
