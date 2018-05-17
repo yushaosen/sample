20112358141a9a19840bdfdcc826b3ecd95ffc1d
yushaosen <yushaosen@163.com>
2018-05-17 14:50:42 +0800

    href

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@
 // });
 
 Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
+Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about');
