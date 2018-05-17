5bfd7ee073e26e7a2689b41e030c43d4b17d8769
yushaosen <yushaosen@163.com>
2018-05-17 15:06:41 +0800

    herf2

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,12 +3,8 @@
 @section('content')
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
-    <p class="lead">
-      你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
-    </p>
-    <p>
-      一切，将从这里开始。
-    </p>
+    <p class="lead">屌丝</p>
+    <p>屌杠</p>
     <p>
       <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
     </p>
