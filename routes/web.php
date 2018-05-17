a3eea3911f8341a92cc360c9179e232e95e3d211
yushaosen <yushaosen@163.com>
2018-05-17 17:19:25 +0800

    注册

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+//注册
+Route::get('signup','UsersController@create')->name('signup');
